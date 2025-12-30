7c9cf19177c13a4da4d57c176a7ad4ca0ce88360
Ch-note <chaehun61@gmail.com>
2025-12-31 06:23:27 +0900

    test_run

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
-
 import yaml
-
 
 with open("config.yaml", "r", encoding="utf-8") as f:
     cfg = yaml.safe_load(f)
-    
-
 
 INPUT_DIR = cfg["INPUT_DIR"]
 OUTPUT_DIR = cfg["OUTPUT_DIR"]
@@ -14,4 +10,6 @@ LANGUAGE = cfg["LANGUAGE"]
 CHUNK_SEC = cfg["CHUNK_SEC"]
 OVERLAP = 3.0
 NUM_WORKERS = 1
-DEVICE = cfg["DEVICE"]
+DEVICE = cfg["DEVICE"]  # GPU(CUDA) 강제 사용
+
+
